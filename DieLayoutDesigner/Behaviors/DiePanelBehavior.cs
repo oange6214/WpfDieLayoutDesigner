b67bb58a340e1ded8b101d261507b0acb875c7be
oange6214 <oange6214@gmail.com>
2024-11-06 10:13:57 +0800

    refactor DiePanelBehavior

--- DieLayoutDesigner/Behaviors/DiePanelBehavior.cs
+++ DieLayoutDesigner/Behaviors/DiePanelBehavior.cs
@@ -7,11 +7,45 @@ namespace DieLayoutDesigner.Behaviors;
 
 public class DiePanelBehavior : Behavior<Canvas>
 {
-    private bool _isPanning;
-    private Point _lastPanPosition;
-    private ICoordinateSystem? _coordinateSystem;
 
-    // Transform Properties
+    #region Fields
+
+    public static readonly DependencyProperty MouseDownCommandProperty =
+        DependencyProperty.Register(
+            nameof(MouseDownCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
+    public static readonly DependencyProperty MouseMoveCommandProperty =
+        DependencyProperty.Register(
+            nameof(MouseMoveCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
+    public static readonly DependencyProperty MouseUpCommandProperty =
+        DependencyProperty.Register(
+            nameof(MouseUpCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
+    public static readonly DependencyProperty PanEndCommandProperty =
+        DependencyProperty.Register(
+            nameof(PanEndCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
+    public static readonly DependencyProperty PanningCommandProperty =
+        DependencyProperty.Register(
+            nameof(PanningCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
+    public static readonly DependencyProperty PanStartCommandProperty =
+        DependencyProperty.Register(
+            nameof(PanStartCommand),
+            typeof(ICommand),
+            typeof(DiePanelBehavior));
+
     public static readonly DependencyProperty ScaleValueProperty =
         DependencyProperty.Register(
             nameof(ScaleValue),
@@ -39,61 +73,13 @@ public class DiePanelBehavior : Behavior<Canvas>
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
                 OnTransformChanged));
 
-    // Mouse Draw Commands
-    public static readonly DependencyProperty MouseDownCommandProperty =
-        DependencyProperty.Register(
-            nameof(MouseDownCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
+    private ICoordinateSystem? _coordinateSystem;
+    private bool _isPanning;
+    private Point _lastPanPosition;
 
-    public static readonly DependencyProperty MouseMoveCommandProperty =
-        DependencyProperty.Register(
-            nameof(MouseMoveCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
+    #endregion Fields
 
-    public static readonly DependencyProperty MouseUpCommandProperty =
-        DependencyProperty.Register(
-            nameof(MouseUpCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
-
-    // Pan Commands
-    public static readonly DependencyProperty PanStartCommandProperty =
-        DependencyProperty.Register(
-            nameof(PanStartCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
-
-    public static readonly DependencyProperty PanningCommandProperty =
-        DependencyProperty.Register(
-            nameof(PanningCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
-
-    public static readonly DependencyProperty PanEndCommandProperty =
-        DependencyProperty.Register(
-            nameof(PanEndCommand),
-            typeof(ICommand),
-            typeof(DiePanelBehavior));
-
-    public double ScaleValue
-    {
-        get => (double)GetValue(ScaleValueProperty);
-        set => SetValue(ScaleValueProperty, value);
-    }
-
-    public double XOffset
-    {
-        get => (double)GetValue(XOffsetProperty);
-        set => SetValue(XOffsetProperty, value);
-    }
-
-    public double YOffset
-    {
-        get => (double)GetValue(YOffsetProperty);
-        set => SetValue(YOffsetProperty, value);
-    }
+    #region Properties
 
     public ICommand MouseDownCommand
     {
@@ -113,10 +99,10 @@ public class DiePanelBehavior : Behavior<Canvas>
         set => SetValue(MouseUpCommandProperty, value);
     }
 
-    public ICommand PanStartCommand
+    public ICommand PanEndCommand
     {
-        get => (ICommand)GetValue(PanStartCommandProperty);
-        set => SetValue(PanStartCommandProperty, value);
+        get => (ICommand)GetValue(PanEndCommandProperty);
+        set => SetValue(PanEndCommandProperty, value);
     }
 
     public ICommand PanningCommand
@@ -125,10 +111,45 @@ public class DiePanelBehavior : Behavior<Canvas>
         set => SetValue(PanningCommandProperty, value);
     }
 
-    public ICommand PanEndCommand
+    public ICommand PanStartCommand
     {
-        get => (ICommand)GetValue(PanEndCommandProperty);
-        set => SetValue(PanEndCommandProperty, value);
+        get => (ICommand)GetValue(PanStartCommandProperty);
+        set => SetValue(PanStartCommandProperty, value);
+    }
+
+    public double ScaleValue
+    {
+        get => (double)GetValue(ScaleValueProperty);
+        set => SetValue(ScaleValueProperty, value);
+    }
+
+    public double XOffset
+    {
+        get => (double)GetValue(XOffsetProperty);
+        set => SetValue(XOffsetProperty, value);
+    }
+
+    public double YOffset
+    {
+        get => (double)GetValue(YOffsetProperty);
+        set => SetValue(YOffsetProperty, value);
+    }
+
+    #endregion Properties
+
+    #region Methods
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        AttachEventHandlers();
+        InitializeCoordinateSystem();
+    }
+
+    protected override void OnDetaching()
+    {
+        DetachEventHandlers();
+        base.OnDetaching();
     }
 
     private static void OnTransformChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -137,14 +158,8 @@ public class DiePanelBehavior : Behavior<Canvas>
         behavior.UpdateCoordinateSystem();
     }
 
-    private void UpdateCoordinateSystem()
+    private void AttachEventHandlers()
     {
-        _coordinateSystem = new ScaledCoordinateSystem(ScaleValue, new Point(XOffset, YOffset));
-    }
-
-    protected override void OnAttached()
-    {
-        base.OnAttached();
         AssociatedObject.MouseLeftButtonDown += OnMouseLeftButtonDown;
         AssociatedObject.MouseMove += OnMouseMove;
         AssociatedObject.MouseLeftButtonUp += OnMouseLeftButtonUp;
@@ -152,11 +167,9 @@ public class DiePanelBehavior : Behavior<Canvas>
         AssociatedObject.PreviewMouseDown += OnPreviewMouseDown;
         AssociatedObject.PreviewMouseUp += OnPreviewMouseUp;
         AssociatedObject.PreviewMouseMove += OnPreviewPanMove;
-
-        _coordinateSystem = new ScaledCoordinateSystem(ScaleValue, new Point(XOffset, YOffset));
     }
 
-    protected override void OnDetaching()
+    private void DetachEventHandlers()
     {
         AssociatedObject.MouseLeftButtonDown -= OnMouseLeftButtonDown;
         AssociatedObject.MouseMove -= OnMouseMove;
@@ -165,121 +178,124 @@ public class DiePanelBehavior : Behavior<Canvas>
         AssociatedObject.PreviewMouseDown -= OnPreviewMouseDown;
         AssociatedObject.PreviewMouseUp -= OnPreviewMouseUp;
         AssociatedObject.PreviewMouseMove -= OnPreviewPanMove;
-        base.OnDetaching();
     }
 
-    // 處理繪圖相關的滑鼠事件
     private Point GetTransformedPosition(MouseEventArgs e)
     {
         var position = e.GetPosition(AssociatedObject);
         return _coordinateSystem?.ToLogical(position) ?? position;
     }
 
+    private void InitializeCoordinateSystem()
+    {
+        _coordinateSystem = new ScaledCoordinateSystem(ScaleValue, new Point(XOffset, YOffset));
+    }
     private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
     {
-        if (!_isPanning && MouseDownCommand?.CanExecute(null) == true)
+        if (_isPanning) 
+            return;
+
+        if (MouseDownCommand?.CanExecute(null) == true)
         {
             var position = GetTransformedPosition(e);
             MouseDownCommand.Execute(position);
+            e.Handled = true;
+        }
+    }
+
+    private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (_isPanning) 
+            return;
+
+        if (MouseUpCommand?.CanExecute(null) == true)
+        {
+            var position = GetTransformedPosition(e);
+            MouseUpCommand.Execute(position);
+            e.Handled = true;
         }
     }
 
     private void OnMouseMove(object sender, MouseEventArgs e)
     {
-        if (!_isPanning && MouseMoveCommand?.CanExecute(null) == true)
+        if (_isPanning) 
+            return;
+
+        if (MouseMoveCommand?.CanExecute(null) == true)
         {
             var position = GetTransformedPosition(e);
             MouseMoveCommand.Execute(position);
         }
     }
 
-    private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-    {
-        if (!_isPanning && MouseUpCommand?.CanExecute(null) == true)
-        {
-            var position = GetTransformedPosition(e);
-            MouseUpCommand.Execute(position);
-        }
-    }
-
     // 處理平移相關的滑鼠事件
     private void OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
     {
-        if (e.MiddleButton == MouseButtonState.Pressed)
-        {
-            _lastPanPosition = e.GetPosition(AssociatedObject);
-            _isPanning = true;
-            AssociatedObject.Cursor = Cursors.Hand;
-            Mouse.Capture((IInputElement)e.OriginalSource);
+        if (e.MiddleButton != MouseButtonState.Pressed) 
+            return;
 
-            if (PanStartCommand?.CanExecute(e) == true)
-            {
-                PanStartCommand.Execute(e);
-            }
+        _lastPanPosition = e.GetPosition(AssociatedObject);
+        _isPanning = true;
+        AssociatedObject.Cursor = Cursors.Hand;
+        Mouse.Capture((IInputElement)e.OriginalSource);
 
-            e.Handled = true;
-        }
-    }
-
-    private void OnPreviewPanMove(object sender, MouseEventArgs e)
-    {
-        if (_isPanning)
-        {
-            var currentPosition = e.GetPosition(AssociatedObject);
-            var delta = currentPosition - _lastPanPosition;
-            _lastPanPosition = currentPosition;
-
-            XOffset += delta.X;
-            YOffset += delta.Y;
-
-            if (PanningCommand?.CanExecute(delta) == true)
-            {
-                PanningCommand.Execute(delta);
-            }
-
-            e.Handled = true;
-        }
+        PanStartCommand?.Execute(e);
+        e.Handled = true;
     }
 
     private void OnPreviewMouseUp(object sender, MouseButtonEventArgs e)
     {
-        if (e.MiddleButton == MouseButtonState.Released && _isPanning)
-        {
-            _isPanning = false;
-            AssociatedObject.Cursor = Cursors.Arrow;
-            Mouse.Capture(null);
+        if (!_isPanning || e.MiddleButton != MouseButtonState.Released) 
+            return;
 
-            if (PanEndCommand?.CanExecute(e) == true)
-            {
-                PanEndCommand.Execute(e);
-            }
+        _isPanning = false;
+        AssociatedObject.Cursor = Cursors.Arrow;
+        Mouse.Capture(null);
 
-            e.Handled = true;
-        }
+        PanEndCommand?.Execute(e);
+        e.Handled = true;
     }
 
     private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
     {
         e.Handled = true;
 
-        Point mousePosition = e.GetPosition(AssociatedObject);
-
-        double zoomFactor = e.Delta > 0 ? 1.2 : 1 / 1.2;
-        double newScale = Math.Clamp(ScaleValue * zoomFactor, 0.1, 10.0);
+        var mousePosition = e.GetPosition(AssociatedObject);
+        var zoomFactor = e.Delta > 0 ? 1.2 : 1 / 1.2;
+        var newScale = Math.Clamp(ScaleValue * zoomFactor, 0.1, 10.0);
 
         if (Math.Abs(newScale - ScaleValue) < 0.001) return;
 
-        double mouseX = mousePosition.X;
-        double mouseY = mousePosition.Y;
+        UpdateZoom(mousePosition, newScale);
+    }
 
-        double oldX = XOffset;
-        double oldY = YOffset;
+    private void OnPreviewPanMove(object sender, MouseEventArgs e)
+    {
+        if (!_isPanning)
+            return;
 
-        double newX = mouseX - (mouseX - oldX) * newScale / ScaleValue;
-        double newY = mouseY - (mouseY - oldY) * newScale / ScaleValue;
+        var currentPosition = e.GetPosition(AssociatedObject);
+        var delta = currentPosition - _lastPanPosition;
+        _lastPanPosition = currentPosition;
 
-        XOffset = newX;
-        YOffset = newY;
+        PanningCommand?.Execute(delta);
+        e.Handled = true;
+    }
+
+    private void UpdateCoordinateSystem()
+    {
+        _coordinateSystem = new ScaledCoordinateSystem(ScaleValue, new Point(XOffset, YOffset));
+    }
+
+    private void UpdateZoom(Point mousePosition, double newScale)
+    {
+        var oldX = XOffset;
+        var oldY = YOffset;
+
+        XOffset = mousePosition.X - (mousePosition.X - oldX) * newScale / ScaleValue;
+        YOffset = mousePosition.Y - (mousePosition.Y - oldY) * newScale / ScaleValue;
         ScaleValue = newScale;
     }
+
+    #endregion Methods
 }
